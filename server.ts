d7c13d8e0fa110381ae579fd9ecc36e67a9e3228
fingerclic-debug <fingerclic@gmail.com>
2026-09-14 15:23:57 +0100

    Fix OAuth token endpoint form parsing

--- server.ts
+++ server.ts
@@ -22,6 +22,7 @@ app.use(securityHeaders);
 app.use(globalRateLimiter);
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Mount OIDC & OAuth Standard Protocol Routes
 app.use('/', oauthRouter);
